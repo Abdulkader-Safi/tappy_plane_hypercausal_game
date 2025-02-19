1e86dbea2966951e0ddb03a58a75bd9b132139ac
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2025-02-19 22:59:37 +0300

    Refactor background movement logic and add main script for dynamic object handling

--- Scripts/background.gd
+++ Scripts/background.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-@export var speed: int = 700
-
-func _process(delta):
-	position.x -= delta * speed
-
+func _process(_delta):
 	if position.x <= -1600:
 		position.x = 0
---
+++ Scripts/main.gd
@@ -0,0 +1,11 @@
+extends Node
+
+@export var dynamic_object_speed: int = 700
+
+@export var speed: int = 700
+
+
+func _process(delta):
+	# position.x -= delta * dynamic_object_speed
+	for dynamic_object in get_tree().get_nodes_in_group("DynamicObject"):
+		dynamic_object.position.x -= delta * dynamic_object_speed
