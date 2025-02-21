bd6aefe67217ecf85d993fb7e8e8b7a3f61429af
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2025-02-21 22:07:08 +0300

    Add obstacle spawning functionality and related scene updates

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,9 +1,12 @@
 extends Node
 
-@export var dynamic_object_speed: int = 700
-@export var health_decrease: int = 3
-@export var health: float = 100
-@export var score: float = 0.0
+@export var obstacle: PackedScene
+
+var dynamic_object_speed: int = 700
+var health_decrease: int = 3
+var spawned_object_position_x: int = 1700
+var health: float = 100
+var score: float = 0.0
 
 func _process(delta):
 	# position.x -= delta * dynamic_object_speed
@@ -19,3 +22,16 @@ func _process(delta):
 	var decimal_index = formatted_score.find(".")
 	formatted_score = formatted_score.left(decimal_index + 3)
 	$UI/HealthBar/ScoreLabel.text = formatted_score
+
+func _on_spawner_timer_timeout():
+	var random: int = randi() % 2
+	var obstacle_instance: Area2D = obstacle.instantiate()
+	$Obstacles.add_child(obstacle_instance)
+	obstacle_instance.position.x = spawned_object_position_x
+
+	if random == 0:
+		obstacle_instance.position.y = 200
+
+	if random == 1:
+		obstacle_instance.position.y = 800
+		obstacle_instance.rotation_degrees = 180
